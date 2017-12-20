8c155664f342f3b4fc0f27e1be8522ef1da0ef3a
ufhy <metallica.one@live.com>
2017-12-20 12:41:39 +0800

    Setup

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,5 @@
 const merge = require('webpack-merge');
+const webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
 
