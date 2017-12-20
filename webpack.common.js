ba63f0df0d1b6490ac2b96f886622841a147492f
ufhy <metallica.one@live.com>
2017-12-20 12:45:04 +0800

    Setup

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
   entry: {
     app: './src/index.js'
   },
   plugins: [
+    new ManifestPlugin(),
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Production'
@@ -15,5 +17,25 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }, {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      }, {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
+      }, {
+        test: /\.(csv|tsv)$/,
+        use: ['csv-loader']
+      }, {
+        test: /\.xml$/,
+        use: ['xml-loader']
+      }
+    ]
   }
 };
